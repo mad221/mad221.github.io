984bb50732f7c190597099d99c36058ab16e8c1a
mad221 <maximepouzol@gmail.com>
2018-12-29 15:43:32 +0100

    rajout du non sorti boucle

--- java.js
+++ java.js
@@ -1,23 +1,21 @@
 
-
-
-
-
 function myfonction() {
 let a="p";
 let c;
-
 while (a!="sorti")
 {
-
 c=prompt("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please (tape oui)");
 if (c=="oui")
 {
   a="sorti";
 }
-
-
+if (c!="oui")
+{
+  a="sorti";
 }
+}
+
+
 if(c=="oui")
 {
 document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
