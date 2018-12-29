94170df1574fc203ccb9437f836614639277084a
mad221 <maximepouzol@gmail.com>
2018-12-29 15:13:44 +0100

    first

---
+++ java.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+function myfonction() {
+let a="p";
+let c;
+
+while (a!="sorti")
+{
+
+c=prompt("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please (tape oui)");
+if (c=="oui")
+{
+  a="sorti";
+}
+
+
+}
+if(c=="oui")
+{
+document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+}
+window.setTimeout("myfonction()",10000);
