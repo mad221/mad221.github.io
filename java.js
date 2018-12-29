45a5cd8d2be6e4460f80cb9b81da291e5f839d6e
mad221 <maximepouzol@gmail.com>
2018-12-29 17:17:14 +0100

    ajout du  mode psycho avec css et html specefique

--- java.js
+++ java.js
@@ -1,24 +1,46 @@
-
+let i=0;
 function myfonction() {
 let a="p";
 let c;
 while (a!="sorti")
 {
-c=prompt("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please (tape oui)");
-if (c=="oui")
+c=confirm("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+if ( c==true)
 {
   a="sorti";
 }
-if (c!="oui")
+if (c!=true)
 {
   a="sorti";
+  psycho();
 }
 }
 
 
-if(c=="oui")
+if( c==true)
 {
 document.location.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
+
 }
+
+
+
+function psycho()
+{
+let d;
+d=confirm("tu veux activiter le mode foufou ?");
+if (d==true)
+{
+document.location.href="page1.html";
+}
+if(d==false)
+{
+
+  document.location.href="bettermotherfuckingwebsite.html";
+
+}
+}
+
+
 window.setTimeout("myfonction()",10000);
